985eac53239c7d2b0a43772ecbe4112af82688f5
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-17 13:39:33 -0500

    Added Autumn object

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// Create an object that represents your pet.
+
+// 1. Name property with a string value.
+// 2. Species property with a string value.
+// 3. Nicknames property with an array value. Array contains strings.
+// 4. Age property with an integer value
+
+const Autumn = {
+    species: "cat",
+    nicknames: ["Bottom", "Autumn, Autumn, stinky bottom"],
+    age: 17
+}
